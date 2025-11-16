eed2f62fe491e66a56de8b760abb4533ce36287e
Jerome David <programingjd@gmail.com>
2025-11-16 12:21:29 +0100

    nonce generation

--- src/nonce.rs
+++ src/nonce.rs
@@ -2,28 +2,19 @@ use blake2::digest::typenum::U16;
 use blake2::digest::{FixedOutput, Mac};
 use blake2::{Blake2b512, Blake2bMac};
 use hkdf::SimpleHkdf;
-use rand::rngs::StdRng;
-use rand::{RngCore, SeedableRng};
 use std::array::from_fn;
 use std::sync::atomic::{AtomicBool, Ordering};
 
 pub struct NonceProducer<const MAX: usize> {
-    rng: StdRng,
-    generation: u16,
+    pub generation: u16,
     bitset: [AtomicBool; MAX],
     iv: [u8; 32],
     mac_key: [u8; 32],
 }
 
 impl<const MAX: usize> NonceProducer<MAX> {
-    pub fn for_generation(generation: u16, seed: [u8; 32]) -> Self {
-        Self::generate(generation, StdRng::from_seed(seed))
-    }
-    pub fn next(&self) -> Self {
-        Self::generate(
-            self.generation.wrapping_add(1),
-            StdRng::from_rng(&mut self.rng.clone()),
-        )
+    pub fn for_generation(generation: u16, seed: &[u8; 32]) -> Self {
+        Self::generate(generation, seed)
     }
     pub fn nonce(&self) -> Option<(usize, [u8; 16])> {
         let k = self.next_index()?;
@@ -31,6 +22,7 @@ impl<const MAX: usize> NonceProducer<MAX> {
         nonce.copy_from_slice(
             &Blake2bMac::<U16>::new_from_slice(&self.mac_key)
                 .expect("invalid blake2b mac key length")
+                .chain_update(self.generation.to_le_bytes())
                 .chain_update((k as u64).to_le_bytes())
                 .chain_update(self.iv)
                 .finalize_fixed(),
@@ -40,6 +32,7 @@ impl<const MAX: usize> NonceProducer<MAX> {
     pub fn verify(&mut self, k: usize, nonce: &[u8; 16]) -> Option<()> {
         if Blake2bMac::<U16>::new_from_slice(&self.mac_key)
             .expect("invalid blake2b mac key length")
+            .chain_update(self.generation.to_le_bytes())
             .chain_update((k as u64).to_le_bytes())
             .chain_update(self.iv)
             .finalize_fixed()
@@ -62,10 +55,8 @@ impl<const MAX: usize> NonceProducer<MAX> {
         }
         None
     }
-    fn generate(generation: u16, mut rng: StdRng) -> Self {
-        let mut seed = [0u8; 32];
-        rng.fill_bytes(&mut seed);
-        let kdf = SimpleHkdf::<Blake2b512>::new(None, &seed);
+    fn generate(generation: u16, seed: &[u8; 32]) -> Self {
+        let kdf = SimpleHkdf::<Blake2b512>::new(None, seed);
         let mut iv = [0u8; 32];
         kdf.expand(b"initialization vector", &mut iv)
             .expect("invalid kdf length expansion");
@@ -73,7 +64,6 @@ impl<const MAX: usize> NonceProducer<MAX> {
         kdf.expand(b"blake2b mac signing key", &mut mac_key)
             .expect("invalid kdf length expansion");
         Self {
-            rng,
             generation,
             bitset: from_fn(|_| AtomicBool::new(false)),
             iv,
@@ -85,36 +75,32 @@ impl<const MAX: usize> NonceProducer<MAX> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rand::TryRngCore;
+    use rand::rngs::OsRng;
     use std::collections::BTreeSet;
-    use std::fmt::{Formatter, LowerHex};
 
     const MAX: usize = 10_000;
 
     #[test]
     fn test_nonce() {
         let mut seed = [0u8; 32];
-        StdRng::from_os_rng().fill_bytes(&mut seed);
-        let producer = NonceProducer::<MAX>::for_generation(0, seed);
+        let mut rng = OsRng::default();
+        rng.try_fill_bytes(&mut seed)
+            .expect("failed to generate seed");
         let mut set = BTreeSet::new();
-        for i in 0..MAX {
-            let nonce = producer.nonce();
-            assert!(nonce.is_some());
-            let (k, nonce) = nonce.unwrap();
-            assert_eq!(k, i);
-            assert!(set.insert(nonce));
-            println!("{:2x}", Hex(nonce.as_slice()));
-        }
-        assert!(producer.nonce().is_none());
-    }
-
-    struct Hex<'a>(&'a [u8]);
-
-    impl LowerHex for Hex<'_> {
-        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-            for &it in self.0 {
-                write!(f, "{:02x}", it)?;
+        for _ in 0..10 {
+            let generation = OsRng::default()
+                .try_next_u32()
+                .expect("failed to generate generation") as u16;
+            let producer = NonceProducer::<MAX>::for_generation(generation, &seed);
+            for i in 0..MAX {
+                let nonce = producer.nonce();
+                assert!(nonce.is_some());
+                let (k, nonce) = nonce.unwrap();
+                assert_eq!(k, i);
+                assert!(set.insert(nonce));
             }
-            Ok(())
+            assert!(producer.nonce().is_none());
         }
     }
 }
